fef3ff60ce810e317a13b5bce659ecb0fe0ecb0c
Carter Nesbitt <ccnesbitt@gmail.com>
2018-10-20 13:44:10 -0400

    Finished decryption function

--- decryptor.py
+++ decryptor.py
@@ -1,25 +1,48 @@
 from PIL import Image
 import hashlib
-import ./hash
+import hash_image
 def decrypt(directory, pwd):
     """
     This function will decrypt a message inside an image.
     :var directory: string ~ file path to image
     :var pwd: string ~ password used to retrieve encryption. 
     """
-
     pwd_hash = hashlib.sha256()
     pwd_hash.update(pwd.encode('utf-8'))
+
     orig_im = Image.open(directory)
     pixels = list(orig_im.getdata())
     width, height = orig_im.size
     pixels = [pixels[i * width : (i+1) * width] for i in range(height)]
-    sequence = get_indices(pwd)
+    sequence = hash_image.get_indices(key_to_int(pwd_hash.digest()), 100, width, height)
     message = ''
+    bits = ''
+    enc_error_count = 0
     for i in range(len(sequence)):
         row = sequence[i][0]
         col = sequence[i][1]
         pixel = pixels[row][col][i%3]
-        pixel = int(pixel, 2)
+        changed_bits = pixel%4  # last two bits
+        bits += str(int(changed_bits > 1)) + str(changed_bits%2)
+    for bit_idx in range(0, len(bits), 8):
+        ch_bits = bits[bit_idx : bit_idx + 8]
+        asc = int(ch_bits, 2)
+        if asc < 128:
+            message += chr(asc)
+        else:
+            enc_error_count += 1
+    if enc_error_count > 0:
+        print("There were", enc_error_count, "encoding errors.")
+        print("Output may not return expected result")
+    return message
 
+
+def key_to_int(key):
+    st = ""
+    for val in key:
+        st += str(val)
+    return int(st)%(2**32-1)
+
+print(decrypt('./slack-profile-picture.jpeg', 'sister-saster'))
+        
 
---
+++ hash_image.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+
+#Seed is the seed for the RNG
+#n is the number of pixels to change
+#p_row is the number of pixels per row
+#p_col is the number of pixels per column
+def get_indices(seed, n, p_row, p_col):
+    p_total = p_row*p_col
+    if (n > p_total):
+        raise RuntimeError("Number of pixels to change must be <= total number of pixels")
+    np.random.seed(seed)
+    ans = list()
+    for i in range(n):
+        row = np.random.randint(0, p_row)
+        col = np.random.randint(0, p_col)
+        coords = (row, col)
+        ans.append(coords)
+    return ans
