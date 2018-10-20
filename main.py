8d4ea14b310fc8cf5b7c2961793e9c8843edf1de
Tom <tommycollege558@gmail.com>
2018-10-20 01:06:15 -0400

    Loaded picture into python, also converted picture into 3D array of binary strings

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-
-
-
+from PIL import Image
+import numpy as np
 
 
 if __name__ == "__main__":
+	im = Image.open("Jake.jpg")
+	im.show()
 
+	# This gets a 3 dimensional array of RBG data.
+	# First inner array is a row of pixels
+	# Each array in that row represent a pixel in RBG format	
+	rgb_array = np.asarray(im)
+	#print(rgb_array)		
 
-	
+	#Vectorize numpy's function to convert to binary.
+	#This is so we can efficiently convert the whole 3D array
+	binary_repr_vec = np.vectorize(np.binary_repr)
+	binary_array = binary_repr_vec(rgb_array)
+	#print(binary_array)
